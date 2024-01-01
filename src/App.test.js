466e7f0bb1ef1a73893600dc31feaa5f1f26a2b7
Hayatullah Rahnamoon <hayat@freightmark.com.my>
2024-01-01 16:32:15 +0100

    save

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import App from './App';
-import user from '@testing-library/user-event';
-
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
